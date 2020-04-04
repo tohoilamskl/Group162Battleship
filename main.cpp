e63f92b852aad11d6881fef04b90b2f0b713a4d0
u3569063 <jo234563@gmail.com>
2020-04-05 02:27:20 +0800

    merged ww2 themes with the board

--- main.cpp
+++ main.cpp
@@ -1,16 +1,36 @@
+//main.cpp
+//includes: main operation, prompt input, print board, generate random board
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <cmath>
-#include <time.h>
+#include <iomanip>  //setw()
+#include <cstdlib>  //rand(), srand(), system()
+#include <ctime> //time()
+#include <string> //stoi()
+
 using namespace std;
 
+const int boardSize = 10;
+//A structure storing all the boards required
+struct Boards {
+    char player[boardSize][boardSize]; //Full board of player (Player see this)
+    char AI[boardSize][boardSize]; //Full board of AI
+    char playerViewAIBoard[boardSize][boardSize]; // AI board but in player's perspectives (Player see this)
+    char AIViewPlayerBoard[boardSize][boardSize]; // player board but in AI's perspectives
+};
+
+Boards boards;
+
+//A structure stroing all the ships of a country
 struct ships
 {
   string shipclass;
   string name;
 };
 
+//function for stroing all the names of all the countries' ships to the given struct
 void naming(ships arr[50],string s)
 {
   string filename = s +".txt";
@@ -105,6 +125,7 @@ void naming(ships arr[50],string s)
     fin.close();
 }
 
+//A function that generates the ships' names for the player's fleet by the country they inputted
 void genfleet(ships uss[50], ships kms[50], ships hms[50], ships rm[50], ships ijn[50], string country)
 {
   string ac,bb,ca,ss,dd;
@@ -183,24 +204,222 @@ void genfleet(ships uss[50], ships kms[50], ships hms[50], ships rm[50], ships i
 
 }
 
+//Function to print both your board, and opponent's board in your perspectives
+void printBoard() {
+    system("CLS"); //clear screen
+    cout << setw(30) << "Opponent's Board" << setw(52) << "Your Board" << endl;
+    cout << "     0   1   2   3   4   5   6   7   8   9                 0   1   2   3   4   5   6   7   8   9  \n";
+    cout << "   +---+---+---+---+---+---+---+---+---+---+             +---+---+---+---+---+---+---+---+---+---+\n";
+    for (int i = 0; i < boardSize; i++) {
+        //Printing your opponent's board (in your perspective)
+        cout << " " << (char)('A' + i);
+        for (int j = 0; j < boardSize; j++) {
+            cout << " | " << boards.playerViewAIBoard[i][j];
+        }
+        cout << " |          ";
+
+        //Printing your own board
+        cout << " " << (char)('A' + i);
+        for (int j = 0; j < boardSize; j++) {
+            cout << " | " << boards.player[i][j];
+        }
+        cout << " |\n";
+        cout << "   +---+---+---+---+---+---+---+---+---+---+             +---+---+---+---+---+---+---+---+---+---+\n";
+    }
+}
+
+//TEMPORARY PRINT BORAD FUNCTION (For developing stage only) (Will print all 4 boards)  (DELETE BEFORE SUBMISSION!!!!!!!!!!!)
+void tempPrintBoard() {
+    system("CLS"); //clear screen
+    cout << "                                          YOUR Perspective                                  |                                       AI Perspective\n";
+    cout << "               Opponent's Board                                  Your Board                 |                  AI Board                                     Your Board\n";
+    cout << "     0   1   2   3   4   5   6   7   8   9        0   1   2   3   4   5   6   7   8   9     |     0   1   2   3   4   5   6   7   8   9        0   1   2   3   4   5   6   7   8   9  \n";
+    cout << "   +---+---+---+---+---+---+---+---+---+---+    +---+---+---+---+---+---+---+---+---+---+   |   +---+---+---+---+---+---+---+---+---+---+    +---+---+---+---+---+---+---+---+---+---+\n";
+    for (int i = 0; i < boardSize; i++) {
+        //Printing your opponent's board (in your perspective)
+        cout << " " << (char)('A' + i);
+        for (int j = 0; j < boardSize; j++) {
+            cout << " | " << boards.playerViewAIBoard[i][j];
+        }
+        cout << " | ";
+
+        //Printing your own board
+        cout << " " << (char)('A' + i);
+        for (int j = 0; j < boardSize; j++) {
+            cout << " | " << boards.player[i][j];
+        }
+        cout << " |   |";
+
+        cout << " " << (char)('A' + i);
+        for (int j = 0; j < boardSize; j++) {
+            cout << " | " << boards.AI[i][j];
+        }
+        cout << " | ";
+
+        cout << " " << (char)('A' + i);
+        for (int j = 0; j < boardSize; j++) {
+            cout << " | " << boards.AIViewPlayerBoard[i][j];
+        }
+        cout << " |\n";
+        cout << "   +---+---+---+---+---+---+---+---+---+---+    +---+---+---+---+---+---+---+---+---+---+   |   +---+---+---+---+---+---+---+---+---+---+    +---+---+---+---+---+---+---+---+---+---+\n";
+    }
+}
+
+//Function to prompt player input, with input validation
+string playerInput() {
+    string target;
+    int targetRow, targetCol;
+    bool validInput;
+
+    //Input validation
+    while (true) {
+        validInput = false;
+
+        //Prompt input
+        cout << "Please choose a target ('q' to quit): ";
+        getline(cin, target);
+
+        if (target.length() <= 2) {
+            //check whether the player input Q or not (TODO: allow player input other command if have)
+            if (target.length() == 1 && (target[0] == 'Q' || target[0] == 'q')) {
+                validInput = true;
+                return target;
+            }
+            if (((target[0] >= 'A' && target[0] <= 'J') || (target[0] >= 'a' && target[0] <= 'j')) && target[1] >= '0' && target[1] <= '9') {
+                if (target[0] >= 'a' && target[0] <= 'j') {
+                    target[0] = target[0] - 'a' + 'A';
+                }
+                validInput = true;
+            }
+        }
+
+        if (validInput == true) {
+            targetRow = (int)target[0] - 'A';
+            targetCol = (int)target[1] - '0';
+            if (boards.playerViewAIBoard[targetRow][targetCol] == 'X' || boards.playerViewAIBoard[targetRow][targetCol] == '@') { //TODO: if target is equal to attacked and is ship sign
+                cout << "Invalid Input! " << target[0] << target[1] << " already attacked, please choose a non-attacked location!\n";
+                validInput == false;
+                continue;
+            }
+            else {
+                return target;
+            }
+        }
+        cout << "Invalid Input! Please input a character from 'A' to 'Z' followed by an integer 1 - 9! (Nothing in between)\n";
+    }
+
+
+    return target;
+}
+
+//Function to place each ship into the board (Randomly)
+void placeShip(char board[][10], int size) {
+    bool vertical = rand() % 2; //true = vertical;, false = horizontal
+    bool invalidPlacement = true;
+    int rowStart, colStart, row, col;
+
+    if (vertical) {  //if place vertically
+        while (invalidPlacement) {
+            invalidPlacement = false;
+
+            //randomly place ship
+            rowStart = rand() % (10 - size);
+            col = rand() % 10;
+            //Check whether this position overlaps with other ships or not
+            for (int i = rowStart; i < rowStart + size; i++) {
+                if (board[i][col] == 'O') {
+                    invalidPlacement = true;
+                    break;
+                }
+            }
+        }
+        for (int i = rowStart; i < rowStart + size; i++) {
+            board[i][col] = 'O'; //place ship
+        }
+    }
+    else {  //if place horizontally
+        while (invalidPlacement) {
+            invalidPlacement = false;
+
+            //randomly place ship
+            colStart = rand() % (10 - size);
+            row = rand() % 10;
+            //Check whether this position overlaps with other ships or not
+            for (int i = colStart; i < colStart + size; i++) {
+                if (board[row][i] == 'O') {
+                    invalidPlacement = true;
+                    break;
+                }
+            }
+        }
+        for (int i = colStart; i < colStart + size; i++) {
+            board[row][i] = 'O'; //place ship
+        }
+    }
+}
+
+//Function to generate random board (randomly put the ships into the board)
+void generateRandomBoard(char board[][10]) {
+    placeShip(board, 5);  //place the size-5 ship
+    placeShip(board, 4);  //place the size-4 ship
+    placeShip(board, 3);  //place the size-3 ship
+    placeShip(board, 2);  //place the size-2 ship
+}
+
+int main() {
+    string target;
+    int targetRow, targetCol;
+    ships uss[50], kms[50], hms[50], rm[50], ijn[50];
+
+    naming(uss,"USS");
+    naming(kms,"KMS");
+    naming(hms,"HMS");
+    naming(rm,"RM");
+    naming(ijn,"IJN");
+
+    srand(time(NULL));
+
+    generateRandomBoard(boards.player);
+    generateRandomBoard(boards.AI);
+
+    int flag = 0;
+    printBoard();
+    cout<<"To play as the United States, type \"us\" "<<endl;
+    cout<<"To play as Great Britain, type \"gb\" "<<endl;
+    cout<<"To play as German Reich, type \"germany\" "<<endl;
+    cout<<"To play as the Kingdom of Italy, type \"italy\" "<<endl;
+    cout<<"To play as the Empire of Great Japan, type \"japan\" "<<endl;
+    string country;
+    cin>>country;
+    genfleet(uss, kms, hms, rm, ijn, country);
 
 
 
+    while (true) {
+
+        target = playerInput();
 
 
-int main()
-{
-  ships uss[50], kms[50], hms[50], rm[50], ijn[50];
+        //if player enters q/Q, it quits
+        if (target[0] == 'Q' || target[0] == 'q') {
+            break;
+        }
+        targetRow = (int)target[0] - 'A';
+        targetCol = (int)target[1] - '0';
+        if (boards.AI[targetRow][targetCol] == 'O') {  //if hit, mark '@'
+            boards.AI[targetRow][targetCol] = '@';
+            boards.playerViewAIBoard[targetRow][targetCol] = '@';
+        }
+        else {
+            boards.AI[targetRow][targetCol] = 'X';   //if not hit, mark 'X'
+            boards.playerViewAIBoard[targetRow][targetCol] = 'X';
+        }
 
-  naming(uss,"USS");
-  naming(kms,"KMS");
-  naming(hms,"HMS");
-  naming(rm,"RM");
-  naming(ijn,"IJN");
+        printBoard();
+    }
 
-  string country;
-  cin>>country;
-  genfleet(uss, kms, hms, rm, ijn, country);
+
+
 
     return 0;
 }
