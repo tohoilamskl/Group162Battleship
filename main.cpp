2141dfc3603b94e84b0cd14f9e0c775c1b5f889b
u3569063 <jo234563@gmail.com>
2020-04-05 01:31:19 +0800

    added genearating a fleet with real names feature

--- main.cpp
+++ main.cpp
@@ -2,11 +2,205 @@
 #include <fstream>
 #include <sstream>
 #include <cmath>
-
+#include <time.h>
 using namespace std;
+
+struct ships
+{
+  string shipclass;
+  string name;
+};
+
+void naming(ships arr[50],string s)
+{
+  string filename = s +".txt";
+  ifstream fin(filename.c_str());
+
+  if (fin.fail())
+  {
+    cout<<"Error in opening "<<s<<endl;
+    exit(1);
+  }
+
+  string sclass,x;
+  while (fin >> x)
+  {
+    sclass = x;
+    if (sclass == "ac")
+      {
+        for (int i = 0; i < 10; i++)
+        {
+
+          string temp;
+          fin >> temp;
+          if (temp == "end")
+          {
+            break;
+          }
+          arr[i].shipclass = "ac";
+          arr[i].name = s +" " + temp;
+        }
+      }
+    if (sclass == "bb")
+    {
+      for (int i = 10; i < 20; i++)
+      {
+
+        string temp;
+        fin >> temp;
+        if (temp == "end")
+        {
+          break;
+        }
+        arr[i].shipclass = "bb";
+        arr[i].name = s +" " + temp;
+      }
+    }
+    if (sclass == "ca")
+    {
+      for (int i = 20; i < 30; i++)
+      {
+
+        string temp;
+        fin >> temp;
+        if (temp == "end")
+        {
+          break;
+        }
+        arr[i].shipclass = "ca";
+        arr[i].name = s +" " + temp;
+      }
+    }
+    if (sclass == "ss")
+    {
+      for (int i = 30; i < 40; i++)
+      {
+
+        string temp;
+        fin >> temp;
+        if (temp == "end")
+        {
+          break;
+        }
+        arr[i].shipclass = "ss";
+        arr[i].name = s +" " + temp;
+      }
+    }
+    if (sclass == "dd")
+    {
+      for (int i = 40; i < 50; i++)
+      {
+
+        string temp;
+        fin >> temp;
+        if (temp == "end")
+        {
+          break;
+        }
+        arr[i].shipclass = "dd";
+        arr[i].name = s +" " + temp;
+      }
+    }
+  }
+    fin.close();
+}
+
+void genfleet(ships uss[50], ships kms[50], ships hms[50], ships rm[50], ships ijn[50], string country)
+{
+  string ac,bb,ca,ss,dd;
+  int num, num1, num2, num3, num4;
+  srand (time(NULL));
+  if (country == "us")
+  {
+    num = rand() % 10 + 1;
+    ac = uss[num].name;
+    num1 = rand() % 10 + 10;
+    bb = uss[num1].name;
+    num2 = rand() % 10 + 20;
+    ca = uss[num2].name;
+    num3 = rand() % 10 + 30;
+    ss = uss[num3].name;
+    num4 = rand() % 10 + 40;
+    dd = uss[num4].name;
+  }
+  else if (country == "gb")
+  {
+    num = rand() % 8 + 0;
+    ac = hms[num].name;
+    num1 = rand() % 8 + 10;
+    bb = hms[num1].name;
+    num2 = rand() % 10 + 20;
+    ca = hms[num2].name;
+    num3 = rand() % 10 + 30;
+    ss = hms[num3].name;
+    num4 = rand() % 10 + 40;
+    dd = hms[num4].name;
+  }
+  else if (country == "italy")
+  {
+    num = rand() % 2 + 0;
+    ac = rm[num].name;
+    num1 = rand() % 7 + 10;
+    bb = rm[num1].name;
+    num2 = rand() % 7 + 20;
+    ca = rm[num2].name;
+    num3 = rand() % 10 + 30;
+    ss = rm[num3].name;
+    num4 = rand() % 10 + 40;
+    dd = rm[num4].name;
+  }
+  else if (country == "germany")
+  {
+    num = rand() % 1 + 0;
+    ac = kms[num].name;
+    num1 = rand() % 4 + 10;
+    bb = kms[num1].name;
+    num2 = rand() % 6 + 20;
+    ca = kms[num2].name;
+    num3 = rand() % 4 + 30;
+    ss = kms[num3].name;
+    num4 = rand() % 6 + 40;
+    dd = kms[num4].name;
+  }
+  else if (country == "japan")
+  {
+    num = rand() % 8 + 0;
+    ac = ijn[num].name;
+    num1 = rand() % 10 + 10;
+    bb = ijn[num1].name;
+    num2 = rand() % 10 + 20;
+    ca = ijn[num2].name;
+    num3 = rand() % 10 + 30;
+    ss = ijn[num3].name;
+    num4 = rand() % 10 + 40;
+    dd = ijn[num4].name;
+  }
+  cout<<"Your aircraft carrier is: "<<ac<<endl;
+  cout<<"Your battleship is: "<<bb<<endl;
+  cout<<"Your heavy cruiser is: "<<ca<<endl;
+  cout<<"Your submarine is: "<<ss<<endl;
+  cout<<"Your destroyer is: "<<dd<<endl;
+
+}
+
+
+
+
+
 
 int main()
 {
+  ships uss[50], kms[50], hms[50], rm[50], ijn[50];
+
+  naming(uss,"USS");
+  naming(kms,"KMS");
+  naming(hms,"HMS");
+  naming(rm,"RM");
+  naming(ijn,"IJN");
+
+  string country;
+  cin>>country;
+  genfleet(uss, kms, hms, rm, ijn, country);
 
     return 0;
 }
